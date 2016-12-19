24bdb93bf36d70ac9b9007dc2d9d55dfdd75d774
Karthik Tiruveedhi <karthiktvs@172-16-25-156.DYNAPOOL.NYU.EDU>
2016-12-19 18:05:24 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Streaming-Data-Monitoring-11
-Streaming Data Monitoring - 11
+# cloudlines
+
+Brief walkthrough - 
+
+type npm start
+what happens?
+    server starts, stream.json keeps getting updated every 10 seconds..
+
+hit url - http://localhost:3000/
+what is shown? 
+    monitoring app with debug details.
+    can view global variables historical_data or stream_data on console.
+    
+How api works? returns recent most 100 mentions. pass a last_id value, returns 100 mentions since tht id.
+Database? We could upload data to ES instance, etc. or just pick from json file for now.
---
+++ index.js
@@ -0,0 +1,120 @@
+let request = require('request'),
+    _ = require('lodash'),
+    jsonfile = require('jsonfile'),
+    bodyParser = require('body-parser');
+
+let config = require('./config');
+jsonfile.spaces = 4;
+
+let runHistorical = () => {
+    console.log('starting historical data');
+    let url = config.api_host;
+    let headers = {
+        'Content-Type': 'application/json', 
+        Authorization: 'Token token="'+config.api_token+'"'
+    }
+    let options = {url, headers}
+    fetchHistoricalData(options);
+}
+
+let fetchHistoricalData = (options) => {
+    console.log('fetch data');
+    //https://github.com/request/request#custom-http-headers
+    request(options, function (error, response, body) {
+        if (!error && body) {
+            try {
+                let results = JSON.parse(body);
+                let last_result = results[results.length - 1];
+                let last_id = last_result.id - 100;
+                console.log(last_id, results.length)
+                let next_options = _.merge(options, {url: config.api_host + '?last_id=' + last_id});
+                var file = './history.json';
+                //https://github.com/jprichardson/node-jsonfile#writefilefilename-obj-options-callback
+                
+                //IMP: Either append and update single json file OR OR OR bulk upload to ES
+                jsonfile.readFile(file, function(err, old_results) {
+                    let updated_results = (old_results) ? _.concat(old_results, results) : results;
+                    console.log('historical data size',updated_results.length);
+                    jsonfile.writeFile(file, updated_results, function (json_err) {
+                        if (json_err) console.error(json_err);
+                        fetchHistoricalData(next_options);
+                    });
+                });
+                
+            } catch(err) {
+                console.log("We’ve encountered parsing json error?: " + err); //couldnt parse, body not proper json
+            }
+        } else {
+            console.log("We’ve encountered an error: " + error);
+        }
+    });
+}
+
+let runStream = () => {
+    console.log('starting stream');
+    let url = config.api_host;
+    let headers = {
+        'Content-Type': 'application/json', 
+        Authorization: 'Token token="'+config.api_token+'"'
+    }
+    let options = {url, headers}
+    //Reset stream data
+    jsonfile.writeFileSync('./stream.json', []); 
+    fetchStreamData(options);
+}
+
+let fetchStreamData = (options) => {
+    console.log('fetch data - ', options.url);
+    //https://github.com/request/request#custom-http-headers
+    request(options, function (error, response, body) {
+        if (!error && body) {
+            try {
+                let results = JSON.parse(body);
+                let most_recent_result = results[0]; //first 
+                let next_options;
+                if (most_recent_result) {
+                    let last_id = most_recent_result.id;
+                    next_options = _.merge(options, {url: config.api_host + '?last_id=' + last_id});
+                } else {
+                    //no results
+                    next_options = options;
+                }
+                
+                console.log(results.length);
+                var time_period = 10000;
+                //https://github.com/jprichardson/node-jsonfile#writefilefilename-obj-options-callback
+                
+                //TODO: check empty results response and see if this code keeps repeating
+                if (results.length > 0) {
+                    var file = './stream.json';
+                    jsonfile.readFile(file, function(err, old_results) {
+                        let updated_results = (old_results) ? _.concat(results, old_results) : results;
+                        console.log('streamed data size',updated_results.length);
+                        jsonfile.writeFile(file, updated_results, function (json_err) {
+                            if (json_err) console.error(json_err);
+                            setTimeout(() => {fetchStreamData(next_options)}, time_period);
+                        });
+                    })
+                } else {
+                    setTimeout(() => {fetchStreamData(next_options)}, time_period);
+                }
+
+                
+            } catch(err) {
+                console.log("We’ve encountered parsing json error?: " + err); //couldnt parse, body not proper json
+            }
+        } else {
+            console.log("We’ve encountered an error: " + error);
+        }
+    });
+}
+
+//Storing in json files allow us to avoid express app. We just periodically pick up from stream.json file.
+// Storing in ES lets us periodically query for recent most data. Requires express app, etc. 
+//runHistorical();
+//runStream();
+
+module.exports = {
+    runHistorical,
+    runStream
+};
